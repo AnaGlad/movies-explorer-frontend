288570f657d4356ef7af8231bca5ff7bfa199292
Anastasia Gladkikh <avpspb117@gmail.com>
2023-09-13 23:39:21 +0300

    исправление замечаний 2

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -9,7 +9,7 @@ function Portfolio({
       <h3 className='portfolio__heading'>Портфолио</h3>
       <ul className='portfolio__link-list'>
         <li className='portfolio__link'>
-          <a href='https://anaglad.github.io/russian-travel/' className='portfolio__link-name' target='_blank' rel="noreferrer">Статичный сайт
+          <a href='https://anaglad.github.io/how-to-learn/' className='portfolio__link-name' target='_blank' rel="noreferrer">Статичный сайт
           <img
             className='portfolio__link-arrow'
             src={arrow}
